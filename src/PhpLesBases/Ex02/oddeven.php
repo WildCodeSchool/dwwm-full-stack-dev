67196bdbb1005f08cc0dc75a06a8ab8cb3a7323d
Manooonlb <[manonlambert.lb@gmail.com](mailto:manonlambert.lb@gmail.com)>
2023-04-24 11:42:35 +0200

    correction exo1

--- src/PhpLesBases/Ex02/oddeven.php
+++ src/PhpLesBases/Ex02/oddeven.php
@@ -1,13 +1,14 @@
 <?php
-while (true){
-$nombre = readline("Entrez un nombre:");
-if (!empty ($nombre) && preg_match("/^[0-9]+$/",$nombre))
-    {
-            if ($nombre%2 == 0) {
-                echo "Le chiffre $nombre est pair \n";
-            } else echo "Le chiffre $nombre est impair \n"; 
-     
-    }  else echo "'$nombre' n'est pas un chiffre \n";
-}
 
-?>
+while (true) {
+    $nombre = readline('Entrez un nombre:');
+    if (!empty($nombre) && preg_match('/^[0-9]+$/', $nombre)) {
+        if (0 == $nombre % 2) {
+            echo "Le chiffre {$nombre} est pair \n";
+        } else {
+            echo "Le chiffre {$nombre} est impair \n";
+        }
+    } else {
+        echo "'{$nombre}' n'est pas un chiffre \n";
+    }
+}
