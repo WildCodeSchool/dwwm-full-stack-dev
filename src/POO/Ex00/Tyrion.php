6706b4f3f004f13e1e2ca3986d992121be8ed407
Océ <oceane.chabrerie.33@gmail.com>
2023-04-24 09:38:31 +0200

    Tyrion class added (Ex00)

---
+++ src/POO/Ex00/Tyrion.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\POO\Ex00;
+
+use App\Resources\Classes\Lannister\Lannister;
+
+class Tyrion extends Lannister
+{
+    public const SIZE = 'Short';
+    public function __construct()
+    {
+        parent::__construct();
+        echo "My name is Tyrion\n";
+    }
+}
