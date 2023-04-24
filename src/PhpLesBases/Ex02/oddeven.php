d86000bf3216fc607963a1d130f432218b87dbf0
Manooonlb <[manonlambert.lb@gmail.com](mailto:manonlambert.lb@gmail.com)>
2023-04-24 10:19:01 +0200

    Ex01 Phplesbases

---
+++ src/PhpLesBases/Ex02/oddeven.php
@@ -0,0 +1,11 @@
+<?php
+
+
+?>
+<html>
+        <form action="oddeven.php" method="post">
+        Entrez un nombre :<input type ="text" name = "nombre" value = ""/>
+        <input type ="submit" name = "validation" value = "ok" />
+        </form>
+</html>
+
