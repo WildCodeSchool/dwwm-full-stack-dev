320e29cba0725ed0124e205469e747aa57482a1d
Manooonlb <[manonlambert.lb@gmail.com](mailto:manonlambert.lb@gmail.com)>
2023-04-24 11:25:49 +0200

    PhpLesBases Ex02

--- src/PhpLesBases/Ex02/oddeven.php
+++ src/PhpLesBases/Ex02/oddeven.php
@@ -1,11 +1,13 @@
 <?php
-
+while (true){
+$nombre = readline("Entrez un nombre:");
+if (!empty ($nombre) && preg_match("/^[0-9]+$/",$nombre))
+    {
+            if ($nombre%2 == 0) {
+                echo "Le chiffre $nombre est pair \n";
+            } else echo "Le chiffre $nombre est impair \n"; 
+     
+    }  else echo "'$nombre' n'est pas un chiffre \n";
+}
 
 ?>
-<html>
-        <form action="oddeven.php" method="post">
-        Entrez un nombre :<input type ="text" name = "nombre" value = ""/>
-        <input type ="submit" name = "validation" value = "ok" />
-        </form>
-</html>
-
