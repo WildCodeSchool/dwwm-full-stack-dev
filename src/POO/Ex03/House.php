1398d1205fdef64fd0ee7708ab2e29cb1de68f32
Océ <oceane.chabrerie.33@gmail.com>
2023-04-25 08:55:10 +0200

    sniffer fixes

--- src/POO/Ex03/House.php
+++ src/POO/Ex03/House.php
@@ -5,11 +5,13 @@ namespace App\POO\Ex03;
 abstract class House
 {
     abstract public function getHouseName(): string;
+
     abstract public function getHouseMotto(): string;
+
     abstract public function getHouseSeat(): string;
 
     public function introduce(): void
     {
-        echo "House " . $this->getHouseName() . " of " . $this->getHouseSeat() . ' : "' . $this->getHouseMotto() . '"' . "\n";
+        echo 'House ' . $this->getHouseName() . ' of ' . $this->getHouseSeat() . ' : "' . $this->getHouseMotto() . '"' . "\n";
     }
 }
