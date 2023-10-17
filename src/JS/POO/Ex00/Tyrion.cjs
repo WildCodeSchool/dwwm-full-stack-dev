fe66eae93a1aa8894d413c6fe673098b5ad62685
Fred <druetfrederique27@gmail.com>
2023-10-17 12:43:45 +0200

    finish exo00

---
+++ src/JS/POO/Ex00/Tyrion.cjs
@@ -0,0 +1,16 @@
+const {
+  Lannister,
+} = require("../../../../resources/JS/Classes/Lannister/Lannister.cjs");
+
+class Tyrion extends Lannister {
+  SIZE = "Short";
+  sayHello() {
+    return `My name is Tyrion`;
+  }
+  announceBirth() {
+    super.announceBirth();
+    console.log(this.sayHello());
+  }
+}
+
+exports.Tyrion = Tyrion;
