75bcb58661c69fd75a42dbfe94dc6a44a32bb4d6
Claire Vannier <clairevannierpro@gmail.com>
2023-11-13 12:03:55 +0100

    fix country column size

--- src/Bdd/Mysql/Quest-1/quest-1_2.sql
+++ src/Bdd/Mysql/Quest-1/quest-1_2.sql
@@ -7,7 +7,7 @@ CREATE TABLE school (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
   capacity INT NULL,
-  country VARCHAR(225) NOT NULL,
+  country VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`)
 );
 
