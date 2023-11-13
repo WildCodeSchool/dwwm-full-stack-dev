3229751e211eb4756c4ca68c038c9ccf0241daa4
Claire Vannier <clairevannierpro@gmail.com>
2023-11-13 11:57:36 +0100

    finish the challenge

--- src/Bdd/Mysql/Quest-1/quest-1_2.sql
+++ src/Bdd/Mysql/Quest-1/quest-1_2.sql
@@ -1,11 +1,13 @@
+USE wild_db_quest;
+
 ALTER TABLE wizard
 ADD is_muggle BOOLEAN NOT NULL;
 
-CREATE TABLE `school` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100) NOT NULL,
-  `capacity` INT NULL,
-  `country` VARCHAR(225) NOT NULL,
+CREATE TABLE school (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  capacity INT NULL,
+  country VARCHAR(225) NOT NULL,
   PRIMARY KEY (`id`)
 );
 
