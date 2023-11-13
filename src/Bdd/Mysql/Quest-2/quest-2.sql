918234216993d1472f76c24be6a00ae2c495ee04
Claire Vannier <clairevannierpro@gmail.com>
2023-11-13 13:30:53 +0100

    fix bug challenge 2

--- src/Bdd/Mysql/Quest-2/quest-2.sql
+++ src/Bdd/Mysql/Quest-2/quest-2.sql
@@ -2,4 +2,8 @@ USE wild_db_quest;
 
 SELECT * FROM wizard WHERE birthday BETWEEN '1975-01-01' AND '1985-12-31';
 
-SELECT firstname FROM wizard WHERE firstname LIKE 'H$';
+SELECT firstname FROM wizard WHERE firstname LIKE 'H%';
+
+SELECT firstname, lastname FROM wizard WHERE lastname = 'POTTER' OR is_muggle IS TRUE ORDER BY firstname;
+
+SELECT firstname, lastname, birthday FROM wizard ORDER BY birthday ASC LIMIT 1;
