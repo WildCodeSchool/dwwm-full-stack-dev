f169485b003b7a903f46a2e5c69865822402414a
Fred <druetfrederique27@gmail.com>
2023-11-13 12:11:05 +0100

    correction capacity country

--- src/Bdd/Mysql/Quest-1/quest-1_2.sql
+++ src/Bdd/Mysql/Quest-1/quest-1_2.sql
@@ -3,8 +3,8 @@ ADD is_muggle boolean NOT NULL;
 CREATE TABLE school (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL, 
-  capacity INT NULL,
-  country VARCHAR(250) NOT NULL,
+  `capacity` INT NULL,
+  `country` VARCHAR(250) NOT NULL,
   PRIMARY KEY (`id`)
   );
 SHOW TABLES;
