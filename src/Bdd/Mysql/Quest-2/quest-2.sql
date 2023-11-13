8a029a8bbe6a9412946de7c2f1c08c8b4ffcdf1d
lulb11 <bordeslucas11@gmail.com>
2023-11-13 16:09:54 +0100

    end

---
+++ src/Bdd/Mysql/Quest-2/quest-2.sql
@@ -0,0 +1,21 @@
+
+SELECT *
+FROM wizard
+WHERE
+    birthday BETWEEN '1975-01-01' AND '1985-12-31';
+
+SELECT * FROM wizard WHERE firstname LIKE 'H%';
+
+SELECT firstname, lastname
+FROM wizard
+WHERE lastname LIKE "Potter"
+ORDER BY firstname ASC;
+
+SELECT
+    firstname,
+    lastname,
+    birthday
+FROM wizard
+WHERE birthday
+ORDER BY birthday DESC
+LIMIT 1;
