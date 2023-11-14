664fe87887a9e90a17ca050a55a39cf71420d92a
Adam HEMAMOU <h-mamou@hotmail.fr>
2023-11-14 09:19:13 +0100

    allez stp

--- src/Bdd/Mysql/Quest-2/quest-2.sql
+++ src/Bdd/Mysql/Quest-2/quest-2.sql
@@ -4,7 +4,7 @@ SELECT * FROM wizard WHERE birthday BETWEEN '1975-01-01' AND '1985-12-31' OR is_
  
 SELECT firstname FROM wizard WHERE firstname LIKE 'H%';
 
-SELECT firstname, lastname FROM wizard WHERE lastname = 'Potter' OR is_muggle IS TRUE ORDER BY firstname ASC;
+SELECT firstname, lastname FROM wizard WHERE lastname = 'Potter' OR is_muggle IS TRUE ORDER BY firstname;
 
 SELECT firstname, lastname, birthday FROM wizard ORDER BY birthday ASC LIMIT 1;
 
