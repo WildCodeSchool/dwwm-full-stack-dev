43f5fba8d634b4978bf50b8e23fb83ffafa5bfdd
Adam HEMAMOU <h-mamou@hotmail.fr>
2023-11-14 09:14:01 +0100

    tryhard this.quest

--- src/Bdd/Mysql/Quest-2/quest-2.sql
+++ src/Bdd/Mysql/Quest-2/quest-2.sql
@@ -1,10 +1,10 @@
 USE wild_db_quest;
 
-SELECT * FROM wizard WHERE birthday BETWEEN '1975-01-01' AND '1985-01-01';
+SELECT * FROM wizard WHERE birthday BETWEEN '1975-01-01' AND '1985-12-31' OR is_muggle = FALSE;
  
 SELECT firstname FROM wizard WHERE firstname LIKE 'H%';
 
-SELECT firstname, lastname FROM wizard WHERE lastname = 'Potter' OR is_muggle IS TRUE ORDER BY firstname;
+SELECT firstname, lastname FROM wizard WHERE lastname = 'Potter' OR is_muggle IS TRUE ORDER BY firstname ASC;
 
 SELECT firstname, lastname, birthday FROM wizard ORDER BY birthday ASC LIMIT 1;
 
